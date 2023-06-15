7ec16730929d898bcd3cb257b18ea813efb45995
Shikhar2512 <129857714+Shikhar2512@users.noreply.github.com>
2023-06-15 22:57:46 +0530

    Firebase authentication config in app

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,8 +1,10 @@
 import './sign-in.styles.scss';
-import { signInWithGooglePopup } from '../../utils/firebase/firebase.utils';
+import { signInWithGooglePopup ,createUserDocumentFromAuth} from '../../utils/firebase/firebase.utils';
+
 const SignIn = ()=>{
     const logGoogleUser = async () =>{
-        const response = await signInWithGooglePopup();
+        const {user} = await signInWithGooglePopup();
+        const userDocRef = await createUserDocumentFromAuth(user);
     }
     return(
         <button onClick={logGoogleUser}>Sign In</button>
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -3,7 +3,12 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
-
+import {
+    getFirestore,
+    doc,//get a document insntance 
+    getDoc,//get data from document 
+    setDoc //set data in document
+} from 'firebase/firestore'
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyBZRPFV-IBpfZ0v34Rs_ZoSwnjS8EN8ln0",
@@ -18,8 +23,29 @@ const firebaseConfig = {
 const firebaseApp = initializeApp(firebaseConfig);
 const provider = new GoogleAuthProvider();
 provider.setCustomParameters({
-    prompt:'select_account',
+    prompt: 'select_account',
 });
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const db = getFirestore(); // points to our database
+export const createUserDocumentFromAuth = async (user) => {
+    console.log(user);
+    const userDocRef = doc(db, 'users', user.uid); // instance of particular user but we dont know it exists or not 
+    const userSnapShot = await getDoc(userDocRef); // actual object to check wheather the user exits in DB or not 
+    if (!userSnapShot.exists()) { // id user data doesnot exist create and set doc
+        const { displayName, email } = user;
+        const createdAt = new Date();
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            })
+        }
+        catch (error) {
+            console.log("error in setDoc ",error);
+        }
+    }
+    return userDocRef;
+} 
